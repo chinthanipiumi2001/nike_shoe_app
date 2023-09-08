d30f458966dec27c23f7816ff96991da32a002e1
user <dissanayakepiumi75@gmail.com>
2023-09-08 20:34:02 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:nike_shoe_app/pages/Login%20Page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext conetxt){
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: Color(0xFFCEDDEE)
+      ),
+      routes: {
+        "/":(conetxt) => LoginPage(),
+      }
+    );
+  }
+}
