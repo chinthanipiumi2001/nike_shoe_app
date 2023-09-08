0244c5c37d018bbe37d47d82ee707c87f561a340
user <dissanayakepiumi75@gmail.com>
2023-09-08 21:14:42 +0530

    add image

--- lib/pages/Login Page.dart
+++ lib/pages/Login Page.dart
@@ -12,15 +12,6 @@ class LoginPage extends StatelessWidget{
                 padding: EdgeInsets.symmetric(vertical: 20),
                 child: Image.asset("images/login.png"),
               ),
-              Container(
-                margin: EdgeInsets.symmetric(horizontal: 20),
-                padding: EdgeInsets.symmetric(horizontal: 15),
-                height: 55,
-                decoration: BoxDecoration(
-                  color: Color(0xFF5F9FD),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-              ),
             ],
           ),
         ),
