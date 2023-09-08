b4862356160721e9974f2f50f8ab6cf12c7a20b0
user <dissanayakepiumi75@gmail.com>
2023-09-08 21:11:05 +0530

    add login image

--- lib/pages/Login Page.dart
+++ lib/pages/Login Page.dart
@@ -3,6 +3,28 @@ import 'package:flutter/material.dart';
 class LoginPage extends StatelessWidget{
   @override
   Widget build (BuildContext context){
-    return Scaffold();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 20),
+                child: Image.asset("images/login.png"),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 20),
+                padding: EdgeInsets.symmetric(horizontal: 15),
+                height: 55,
+                decoration: BoxDecoration(
+                  color: Color(0xFF5F9FD),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
