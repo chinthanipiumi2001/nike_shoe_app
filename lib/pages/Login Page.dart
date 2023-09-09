3276df899540440f97dd73e115f82f538d80a1f5
user <dissanayakepiumi75@gmail.com>
2023-09-09 19:26:02 +0530

    update login page

--- lib/pages/Login Page.dart
+++ lib/pages/Login Page.dart
@@ -2,20 +2,9 @@ import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget{
   @override
-  Widget build (BuildContext context){
+  Widget build(BuildContext context){
     return Scaffold(
-      body: SingleChildScrollView(
-        child: SafeArea(
-          child: Column(
-            children: [
-              Padding(
-                padding: EdgeInsets.symmetric(vertical: 20),
-                child: Image.asset("images/login.png"),
-              ),
-            ],
-          ),
-        ),
-      ),
+
     );
   }
 }
