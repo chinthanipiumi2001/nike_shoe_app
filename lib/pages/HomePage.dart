4954b05000643725cef3925f487cf9ae11453984
user <dissanayakepiumi75@gmail.com>
2023-09-22 21:08:48 +0530

    add color codes

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -3,6 +3,34 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget{
   @override
   Widget build (BuildContext context){
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(15),
+                child: Row(children: [
+                  Container(
+                    padding: EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                      color: Color(0xFFF5F9FD),
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Color(0xFF475369).withOpacity(0.3),
+                          blurRadius: 5,
+                          spreadRadius: 1,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
