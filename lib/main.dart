150d01cb782bf24528ad09ed422109af3ef36fc6
user <dissanayakepiumi75@gmail.com>
2023-09-22 20:47:55 +0530

    add file home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nike_shoe_app/pages/HomePage.dart';
 import 'package:nike_shoe_app/pages/Login%20Page.dart';
 
 void main() => runApp(MyApp());
@@ -13,6 +14,7 @@ class MyApp extends StatelessWidget{
       ),
       routes: {
         "/":(conetxt) => LoginPage(),
+        "homePage":(context) => HomePage(),
       }
     );
   }
